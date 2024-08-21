a3d381b8af778d066373c14a9323d9ec2d84c86a
vitor <vitor.gtr@outlook.com>
2024-08-21 08:46:03 -0300

    construindo componente de upload de arquivos

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,8 +1,15 @@
+import { useContext, useState } from 'react';
 import Header from '../../components/Header';
 import Title from '../../components/Title';
-import { FiSettings } from 'react-icons/fi'
+import { FiSettings, FiUpload } from 'react-icons/fi'
+import avatar from '../../assets/avatar.png';
+import { AuthContext } from '../../context/auth';
 
 export default function Profile() {
+
+    const { user } = useContext(AuthContext);
+    
+
     return (
         <div>
             <Header />
@@ -11,8 +18,23 @@ export default function Profile() {
                 <Title name="Minha conta">
                     <FiSettings size={25} />
                 </Title>
+                <div className='container'>
+                
+                <form className='form-profile'>
+
+                <label className='label-avatar'>
+                    <label>
+                        <FiUpload color='#fff' size={25}/>
+                    </label>
+
+                    <input type='file' accept='image/*'/> <br />
+                    {}
+                </label>
+                </form>
+                
+                </div>
+
             </div>
-            {/* <h1>Pagina Profile</h1> */}
         </div>
     )
 }
