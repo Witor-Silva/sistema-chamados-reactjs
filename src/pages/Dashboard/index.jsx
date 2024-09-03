265c7d5ae510008f5f2a2e561ea59cbcab97fd68
vitor <vitor.gtr@outlook.com>
2024-09-03 08:52:00 -0300

    listagem e cadastro de chamado

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/auth";
 
 import Header from "../../components/Header/index";
@@ -6,14 +6,56 @@ import Title from "../../components/Title";
 import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from "react-icons/fi";
 
 import { Link } from "react-router-dom";
+import { collection, getDocs, orderBy, limit, startAfter, query } from 'firebase/firestore';
+import { db } from '../../services/firebaseConnection'
 
 import "./dashboard.css";
+
+const listRef = collection(db, "chamados")
 
 export default function Dashboard() {
   const { logout } = useContext(AuthContext);
 
-  async function handleLogout() {
-    await logout();
+  const [chamados, setChamados] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [isEmpty, setIsEmpty] = useState(false);
+
+  useEffect(() => {
+    async function loadChamados() {
+      const q = query(listRef, orderBy('created', 'desc'), limit(5));
+
+      const querySnapshot = await getDocs(q)
+      await updateState(querySnapshot);
+
+      setLoading(false);
+
+    }
+    loadChamados()
+  }, [])
+
+  async function updateState(querySnapshot) {
+    const isCollectionEmpty = querySnapshot.size === 0;
+
+    if (!isCollectionEmpty) {
+      let lista = [];
+
+      querySnapshot.forEach((doc) => {
+        lista.push({
+          id: doc.id,
+          assunto: doc.data().assunto,
+          cliente: doc.data().cliente,
+          clientId: doc.data().clientId,
+          created: doc.data().created,
+          status: doc.data().status,
+          complemento: doc.data().complemento,
+        })
+      });
+
+      setChamados(chamados => [...chamados, ...lista])
+    } else {
+      setIsEmpty(true);
+    }
+
   }
 
   return (
@@ -25,72 +67,66 @@ export default function Dashboard() {
         </Title>
 
         <>
-          <Link to="/new" className="new">
-            <FiPlus color="#fff" size={25} />
-            Novo chamado
-          </Link>
-          <table>
-            <thead>
-              <tr>
-                <th scope="col">Clientes</th>
-                <th scope="col">Assunto</th>
-                <th scope="col">Status</th>
-                <th scope="col">Cadastrado em</th>
-                <th scope="col">#</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td data-label="Cliente">Mercado Esquina</td>
-                <td data-label="Assunto">Suporte</td>
-                <td data-label="Status">
-                  <span className="badge" style={{ backgroundColor: "#999" }}>
-                    Em aberto
-                  </span>
-                </td>
-                <td data-label="Cadastrado">01/09/2024</td>
-                <td data-label="#">
-                  <button
-                    className="action"
-                    style={{ backgroundColor: "#3583f6" }}
-                  >
-                    <FiSearch color="#fff" size={17} />
-                  </button>
-                  <button
-                    className="action"
-                    style={{ backgroundColor: "#f6a935" }}
-                  >
-                    <FiEdit2 color="#fff" size={17} />
-                  </button>
-                </td>
-              </tr>
 
-              <tr>
-                <td data-label="Cliente">Davison Tech</td>
-                <td data-label="Assunto">Suporte</td>
-                <td data-label="Status">
-                  <span className="badge" style={{ backgroundColor: "#999" }}>
-                    Em aberto
-                  </span>
-                </td>
-                <td data-label="Cadastrado">25/08/2024</td>
-                <td data-label="#">
-                  <button
-                    className="action"
-                    style={{ backgroundColor: "#3583f6" }}
-                  >
-                    <FiSearch color="#fff" size={17} />
-                  </button>
-                  <button
-                    className="action"
-                    style={{ backgroundColor: "#f6a935" }}
-                  >
-                    <FiEdit2 color="#fff" size={17} />
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          {chamados.length === 0 ? (
+            <div className="container dashboard">
+              <span>Nenhum chamado encontrado...</span>
+              <Link to="/new" className="new">
+                <FiPlus color="#fff" size={25} />
+                Novo chamado
+              </Link>
+            </div>
+          ) : (
+            <>
+              <Link to="/new" className="new">
+                <FiPlus color="#fff" size={25} />
+                Novo chamado
+              </Link>
+
+              <table>
+                <thead>
+                  <tr>
+                    <th scope="col">Clientes</th>
+                    <th scope="col">Assunto</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Cadastrado em</th>
+                    <th scope="col">#</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td data-label="Cliente">Mercado Esquina</td>
+                    <td data-label="Assunto">Suporte</td>
+                    <td data-label="Status">
+                      <span className="badge" style={{ backgroundColor: "#999" }}>
+                        Em aberto
+                      </span>
+                    </td>
+                    <td data-label="Cadastrado">01/09/2024</td>
+                    <td data-label="#">
+                      <button
+                        className="action"
+                        style={{ backgroundColor: "#3583f6" }}
+                      >
+                        <FiSearch color="#fff" size={17} />
+                      </button>
+                      <button
+                        className="action"
+                        style={{ backgroundColor: "#f6a935" }}
+                      >
+                        <FiEdit2 color="#fff" size={17} />
+                      </button>
+                    </td>
+                  </tr>
+
+
+                </tbody>
+              </table>
+            </>
+          )}
+
+
+
         </>
       </div>
     </div>
--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -5,7 +5,9 @@ import { FiPlusCircle } from "react-icons/fi";
 
 import { AuthContext } from "../../context/auth";
 import { db } from "../../services/firebaseConnection";
-import { collection, getDocs, getDoc, doc } from "firebase/firestore";
+import { collection, getDocs, getDoc, doc, addDoc } from "firebase/firestore";
+
+import { toast } from 'react-toastify';
 
 import "./new.css";
 
@@ -66,6 +68,30 @@ export default function New() {
     setCustomerSelected(e.target.value);
   }
 
+  async function handleRegister(e){
+    e.preventDefault();
+
+    //Registrar um chamado
+    await addDoc(collection(db, "chamados"), {
+      created: new Date(),
+      cliente: customers[customerSelected].nomeFantsaia,
+      clientId: customers[customerSelected].id,
+      assunto: assunto,
+      complemento,
+      status: status,
+      userId: user.uid,
+    })
+    .then(() => {
+      toast.success('CHAMADO REGISTRADO!')
+      setComplemento('')
+      setCustomerSelected(0)
+    })
+    .catch((error) => {
+      toast.error('Ops, erro ao registrar, tente mais tarde!')
+      console.log(error);
+    })
+  }
+
   return (
     <div>
       <Header />
@@ -76,7 +102,7 @@ export default function New() {
         </Title>
 
         <div className="container">
-          <form className="form-profile">
+          <form className="form-profile" onSubmit={handleRegister}>
             <label>Clientes</label>
             {loadCustomer ? (
               <imput type="text" disable={true} value="Carregando ..." />
